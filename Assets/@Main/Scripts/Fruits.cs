47727adf10570cab6ceae36fd936354b69086e20
bsheep <blacksheep.feather@gmail.com>
2021-08-23 10:45:25 +0900

    add score item

---
+++ Assets/@Main/Scripts/Fruits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits : MonoBehaviour
+{
+    public GameObject m_collectedPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Player"))
+        {
+            var collected = Instantiate
+            (
+                m_collectedPrefab,
+                transform.position,
+                Quaternion.identity
+            );
+            var animator = collected.GetComponent<Animator>();
+            var info = animator.GetCurrentAnimatorStateInfo(0);
+            var time = info.length;
+            Destroy(collected, time);
+
+            Destroy(gameObject);
+        }
+    }
+}
