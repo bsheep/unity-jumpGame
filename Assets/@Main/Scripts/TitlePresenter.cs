18bb64de994a9133bfc11c5e45e9869f146c3495
bsheep <blacksheep.feather@gmail.com>
2021-08-25 15:20:34 +0900

    add title-scene

---
+++ Assets/@Main/Scripts/TitlePresenter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlePresenter : MonoBehaviour
+{
+    public TitleView view;
+
+    bool isCursorToStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCursorToStart = true;
+        view.setCursorToStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isCursorToStart = !isCursorToStart;
+            if (isCursorToStart)
+            {
+                view.setCursorToStart();
+            }
+            else
+            {
+                view.setCursorToTutorial();
+            }
+        }
+    }
+}
---
+++ Assets/@Main/Scripts/TitleView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleView : MonoBehaviour
+{
+    public GameObject cursor;
+    Vector3 cursorPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorPos = cursor.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setCursorToStart()
+    {
+        cursorPos.y = -200;
+        cursor.transform.localPosition = cursorPos;
+    }
+
+    public void setCursorToTutorial()
+    {
+        cursorPos.y = -260;
+        cursor.transform.localPosition = cursorPos;
+    }
+}
