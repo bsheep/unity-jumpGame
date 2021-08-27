49929772e3a0cc9be27c49d2f496e75599589b8d
bsheep <blacksheep.feather@gmail.com>
2021-08-27 13:09:59 +0900

    change score text to textmeshpro

--- Assets/@Main/Scripts/StageView.cs
+++ Assets/@Main/Scripts/StageView.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StageView : MonoBehaviour
 {
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
 
     public GameObject mainCamera;
 
